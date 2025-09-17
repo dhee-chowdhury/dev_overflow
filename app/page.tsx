8343b3e972d78fc09875b58ed7700bb81016c37f
dhee-chowdhury <dhee.chowdhury@gmail.com>
2025-09-17 08:42:25 +0600

    add assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="text-4xl text-violet-900 font-black">
-      Welcome to Next.js
+    <div className="">
+      <h1 className="text-4xl">Welcome to Next.js</h1>
+      <h1 className="font-space-grotesk text-4xl">Welcome to Next.js</h1>
     </div>
   );
 };
